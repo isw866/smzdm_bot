9d26c50a528b62ca18048e1d59b21653919de041
isw866 <mleen@vip.qq.com>
2021-01-27 11:15:30 +0800

    Update main.py

--- main.py
+++ main.py
@@ -60,7 +60,6 @@ if __name__ == '__main__':
     print('sc_key: ', SERVERCHAN_SECRETKEY)
     if isinstance(SERVERCHAN_SECRETKEY,str) and len(SERVERCHAN_SECRETKEY)>0:
         print('检测到 SCKEY， 准备推送')
-        push_to_wechat(text = '什么值得买每日签到',
-                        desp = str(res),
+        push_to_wechat(msg = '什么值得买每日签到'+str(res),
                         secretKey = SERVERCHAN_SECRETKEY)
     print('代码完毕')
